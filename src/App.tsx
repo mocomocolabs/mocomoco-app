6c09254ccbfa0cfa7a290e270c41bc69ee9bcae5
junho <nowonlyhere@gmail.com>
2020-09-17 22:43:59 +0900

    feature: bottom tab 및 기본 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import './global.scss'
-
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react'
+import { IonApp, IonIcon, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
+import { basket, beer, home, paperPlane, personCircle } from 'ionicons/icons'
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
-
-import Menu from './components/Menu'
-import News from './pages/example/News'
-import Todo from './pages/example/Todo'
+import './global.scss'
+import Chat from './pages/Chat'
+import Community from './pages/Community'
+import Home from './pages/Home'
+import MyPage from './pages/MyPage'
+import Trade from './pages/Trade'
 
 const App: React.FC = () => {
   return (
     <IonApp>
       <IonReactRouter>
-        <IonSplitPane contentId='main'>
-          <Menu />
+        <IonTabs>
           <IonRouterOutlet id='main'>
-            <Route path='/news' component={News} exact />
-            <Route path='/todo' component={Todo} exact />
-            <Redirect from='/' to='/news' exact />
+            <Route path='/home' component={Home} exact />
+            <Route path='/community' component={Community} exact />
+            <Route path='/trade' component={Trade} exact />
+            <Route path='/chat' component={Chat} exact />
+            <Route path='/my-page' component={MyPage} exact />
+            <Redirect from='/' to='/home' exact />
           </IonRouterOutlet>
-        </IonSplitPane>
+          <IonTabBar slot='bottom'>
+            <IonTabButton tab='home' href='/home'>
+              <IonIcon icon={home} />
+            </IonTabButton>
+            <IonTabButton tab='community' href='/community'>
+              <IonIcon icon={beer} />
+            </IonTabButton>
+            <IonTabButton tab='trade' href='/trade'>
+              <IonIcon icon={basket} />
+            </IonTabButton>
+            <IonTabButton tab='chat' href='/chat'>
+              <IonIcon icon={paperPlane} />
+            </IonTabButton>
+            <IonTabButton tab='my-page' href='/my-page'>
+              <IonIcon icon={personCircle} />
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
       </IonReactRouter>
     </IonApp>
   )
