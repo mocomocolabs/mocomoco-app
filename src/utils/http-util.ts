e4fa95b39220e347142ef9c22eadf783c5282f96
junho <nowonlyhere@gmail.com>
2020-09-18 23:19:41 +0900

    feat: http util add

---
+++ src/utils/http-util.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import config from '../config'
+
+// interface IAxiosResponse {
+//   data: IResponse
+// }
+
+// interface IResponse {
+//   result: any
+//   errorMessage: string
+// }
+
+const _axios = axios.create({
+  baseURL: config.API_URL,
+  withCredentials: false,
+  // transformResponse: (r: IAxiosResponse) => r,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// _axios.interceptors.response.use(
+//   (response) => {
+//     return JSON.parse(response.data)
+//   },
+//   (error) => {
+//     console.log(error)
+
+//     // Do something with response error
+//     return Promise.reject(error)
+//   }
+// )
+
+export default _axios
