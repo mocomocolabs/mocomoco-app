d857cd3ba26fb6a4fbaa31c28d663b86038047fe
junho <nowonlyhere@gmail.com>
2020-09-26 18:12:31 +0900

    style: eslint react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'plugin:react-hooks/recommended', // Enforces the Rules of Hooks
   ],
+  plugins: ['react-hooks'],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
@@ -15,6 +15,9 @@ module.exports = {
     },
   },
   rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
     'react/prop-types': 'off',
     'react/display-name': 'off',
     'no-restricted-syntax': [
