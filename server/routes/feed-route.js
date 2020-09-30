9e111702352169045ff421e76efb8410d5dd333b
junho <nowonlyhere@gmail.com>
2020-09-30 17:44:28 +0900

    feat: 피드 상세 server

--- server/routes/feed-route.js
+++ server/routes/feed-route.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 
-const { feeds } = require('./mock/feed-mock')
+const { feeds, feed } = require('./mock/feed-mock')
 
 router.get('/feeds', (req, res) => {
   res.send(feeds)
+})
+
+router.get('/feeds/:id', (req, res) => {
+  res.send(feed)
 })
 
 module.exports = router
