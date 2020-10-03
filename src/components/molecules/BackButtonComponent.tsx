3fd69f2dc6247038748471417ff7720560ea98a5
gyuon.hwang <onssss@gmail.com>
2020-10-03 21:48:44 +0900

    fix: BackButton에 icon prop 추가

--- src/components/molecules/BackButtonComponent.tsx
+++ src/components/molecules/BackButtonComponent.tsx
@@ -3,13 +3,16 @@ import { closeOutline } from 'ionicons/icons'
 import React, { FC } from 'react'
 import { useHistory } from 'react-router'
 
-export interface IBackButton {}
+export interface IBackButton {
+  icon?: string
+}
 
-export const BackButton: FC<IBackButton> = () => {
+export const BackButton: FC<IBackButton> = ({ icon = closeOutline }) => {
   const history = useHistory()
+
   return (
     <div onClick={() => history.goBack()}>
-      <IonIcon icon={closeOutline} size='large'></IonIcon>
+      <IonIcon icon={icon} size='large'></IonIcon>
     </div>
   )
 }
