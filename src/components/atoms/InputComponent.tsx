ed402c8e02f71348be14d30b514796dd3843d22a
junho <nowonlyhere@gmail.com>
2020-12-06 17:55:41 +0900

    update: Input컴포넌트 native로 변경

--- src/components/atoms/InputComponent.tsx
+++ src/components/atoms/InputComponent.tsx
@@ -1,20 +1,28 @@
-import { IonInput } from '@ionic/react'
 import React, { FC } from 'react'
 
 export interface IInput {
+  name?: string
+  type?: string
   value?: string
+  defaultValue?: string
   placeholder?: string
   onChange?: (v: string) => void
+  // eslint-disable-next-line
+  register?: any
 }
 
-export const Input: FC<IInput> = ({ value, placeholder, onChange }) => {
+export const Input: FC<IInput> = ({ name, type, value, defaultValue, placeholder, onChange, register }) => {
   return (
     <div className='border-border px-3'>
-      <IonInput
+      <input
+        type={type || 'text'}
+        name={name}
         value={value}
+        defaultValue={defaultValue}
         placeholder={placeholder}
-        onIonChange={(e) => onChange && onChange(e.detail.value!)}
-      ></IonInput>
+        onChange={(e) => onChange && onChange(e.target.value!)}
+        ref={register}
+      ></input>
     </div>
   )
 }
