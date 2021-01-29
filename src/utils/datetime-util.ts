828e0d1fe5409ee1b3976e0c3be676d278e0b59a
gyuon <onssss@gmail.com>
2021-01-30 02:05:17 +0900

    feat: show stuff/talent createAt time

--- src/utils/datetime-util.ts
+++ src/utils/datetime-util.ts
@@ -10,3 +10,26 @@ export enum DT_FORMAT {
 export const ymd = (value?: string) => dayjs(value).format(DT_FORMAT.YMD)
 export const hm = (value?: string) => dayjs(value).format(DT_FORMAT.HM)
 export const ymdhm = (value?: string) => dayjs(value).format(DT_FORMAT.YMDHM)
+
+export const timeDiff = (from?: string, to?: string) => {
+  const fromDate = from ? dayjs(from) : dayjs()
+  const toDate = to ? dayjs(to) : dayjs()
+
+  const diffYears = fromDate.diff(toDate, 'year')
+  if (diffYears > 0) return toDate.format('YYYY년 M월 D일')
+
+  const diffDays = fromDate.diff(toDate, 'day')
+  if (diffDays > 6) return toDate.format('M월 D일')
+  if (diffDays > 0) return `${diffDays}일 전`
+
+  const diffHours = fromDate.diff(toDate, 'hour')
+  if (diffHours > 0) return `${diffHours}시간 전`
+
+  const diffMinutes = fromDate.diff(toDate, 'minute')
+  if (diffMinutes > 0) return `${diffMinutes}분 전`
+
+  const diffSeconds = fromDate.diff(toDate, 'second')
+  if (diffSeconds > 0) return `${diffSeconds}초 전`
+
+  return '방금 전'
+}
