ee48c908d508371efce508e0274da0790d277349
gyuon <onssss@gmail.com>
2021-03-26 13:44:58 +0900

    chore/disable eslint warning in task.d.ts

--- src/stores/task.d.ts
+++ src/stores/task.d.ts
@@ -1,6 +1,11 @@
 import { Task as TaskType } from 'mobx-task'
 export type Task = TaskType<[], void>
 
+// eslint-disable-next-line
 export type TaskBy<T extends any> = TaskType<[T], void>
+
+// eslint-disable-next-line
 export type TaskBy2<T extends any, U extends any> = TaskType<[T, U], void>
+
+// eslint-disable-next-line
 export type TaskBy3<T extends any, U extends any, V extends any> = TaskType<[T, U, V], void>
