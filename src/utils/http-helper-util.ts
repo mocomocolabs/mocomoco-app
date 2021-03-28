c45052b357f8ceeed32fbad181785ffb800429f8
junho <nowonlyhere@gmail.com>
2021-03-28 17:19:59 +0900

    feat: http helper, 호출후 에러 토스트, axios response

---
+++ src/utils/http-helper-util.ts
@@ -0,0 +1,16 @@
+import { AxiosError, AxiosResponse } from 'axios'
+import { rootStore } from '../index'
+
+export const executeWithError = (func: () => Promise<any>) => {
+  func().catch((err) => {
+    rootStore.$ui.showToastError({ message: err.message })
+  })
+}
+
+export const responseSuccess = (response: AxiosResponse<any>) => {
+  return response.data ? JSON.parse(response.data) : response.data
+}
+
+export const responseError = (error: AxiosError<any>) => {
+  return error.response?.data ? JSON.parse(error.response.data) : error.response
+}
