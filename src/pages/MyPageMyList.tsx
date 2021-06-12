c410eb3a991c9279e61984f2530ebb17f20c0ba8
gyuon <onssss@gmail.com>
2021-06-12 15:56:25 +0900

    feat: MyList 물건/재능 필터UI

--- src/pages/MyPageMyList.tsx
+++ src/pages/MyPageMyList.tsx
@@ -9,49 +9,51 @@ import {
   IonToolbar,
 } from '@ionic/react'
 import { filterOutline } from 'ionicons/icons'
-import { useObserver } from 'mobx-react-lite'
-import React, { useEffect, useState } from 'react'
+import { reaction } from 'mobx'
+import { Observer } from 'mobx-react-lite'
+import React, { useEffect, useRef, useState } from 'react'
 import { BackButton } from '../components/molecules/BackButtonComponent'
+import { FilterBar } from '../components/molecules/FilterBarComponent'
+import { FilterPopup } from '../components/molecules/FilterPopupComponent'
 import { Segment } from '../components/molecules/SegmentComponent'
 import { ClubOurTownList } from '../components/organisms/ClubOurTownListComponent'
 import { FeedList } from '../components/organisms/FeedListComponent'
 import { StuffTalentList } from '../components/organisms/StuffTalentListComponent'
 import { useStore } from '../hooks/use-store'
 import { ISegments, SEGMENT_KEYS } from '../models/segment.d'
-import { IStuffTalentFilter } from '../models/stufftalent'
+import { IStuffTalentFilter, StuffTalentType } from '../models/stufftalent.d'
 
-// const FilterMode = { none: 'none', category: 'category', status: 'status' }
-// type FilterMode = typeof FilterMode[keyof typeof FilterMode]
-const initialFilter: IStuffTalentFilter = {
-  isPublic: false,
-  communityId: undefined,
-  userId: undefined,
-  categories: [],
-  notStatuses: [],
-  types: [],
+// TODO segment_keys를 stufftalent용으로만 사용하고 있으니, segment.d 파일명을 바꾸던가 해야겠다
+const segments: ISegments = {
+  [SEGMENT_KEYS.stuff]: { label: '물건창고' },
+  [SEGMENT_KEYS.talent]: { label: '재능창고' },
+  [SEGMENT_KEYS.feed]: { label: '이야기창고' },
+  [SEGMENT_KEYS.club]: { label: '소모임' },
 }
+
+const FilterMode = { none: 'none', type: '조건' }
+type FilterMode = typeof FilterMode[keyof typeof FilterMode]
 
 export const MyPageMyList: React.FC = () => {
   const { $segment, $auth, $stuff, $talent, $feed, $club } = useStore()
 
-  // TODO segment_keys를 stufftalent용으로만 사용하고 있으니, segment.d 파일명을 바꾸던가 해야겠다
-  const segments: ISegments = {
-    [SEGMENT_KEYS.stuff]: { label: '물건창고' },
-    [SEGMENT_KEYS.talent]: { label: '재능창고' },
-    [SEGMENT_KEYS.feed]: { label: '이야기창고' },
-    [SEGMENT_KEYS.club]: { label: '소모임' },
-  }
+  const segment = useRef<SEGMENT_KEYS>($segment.mylistSegment)
 
-  useEffect(() => {
-    // TODO clublistcomponent 안으로 옮길지 고민해보자
-    $segment.mylistSegment === SEGMENT_KEYS.club && $club.getMyClubs()
-  }, [$segment.mylistSegment, $club])
-
-  // TODO const [filterMode, setFilterMode] = useState<FilterMode>(FilterMode.none)
-  const [filter, setFilter] = useState<IStuffTalentFilter>({
-    ...initialFilter,
+  const initialFilter: IStuffTalentFilter = {
+    isPublic: false,
+    communityId: undefined,
     userId: $auth.user.id,
-  })
+    categories: [],
+    notStatuses: [],
+    types: [],
+  }
+  const [filter, setFilter] = useState<IStuffTalentFilter>(initialFilter)
+  const [filterMode, setFilterMode] = useState<FilterMode>(FilterMode.none)
+  const onCloseFilterPopup = () => setFilterMode(FilterMode.none)
+  const onResetFilter = () => {
+    setFilter(initialFilter)
+    setFilterMode(FilterMode.none)
+  }
 
   const renderList = (segmentKey: SEGMENT_KEYS) => {
     // TODO 오마이갓.. store/fetchTask/clubs => 파라미터를 통일하자
@@ -69,7 +71,30 @@ export const MyPageMyList: React.FC = () => {
     }
   }
 
-  return useObserver(() => (
+  useEffect(
+    () => {
+      const disposeReaction = reaction(
+        () => $segment.mylistSegment,
+        (selectedSegment) => {
+          if (segment.current !== selectedSegment) {
+            segment.current = selectedSegment
+            onResetFilter()
+          }
+
+          // TODO clublistcomponent 안으로 옮길지 고민해보자
+          selectedSegment === SEGMENT_KEYS.club && $club.getMyClubs()
+        }
+      )
+
+      return () => {
+        disposeReaction()
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
+
+  return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
@@ -78,23 +103,62 @@ export const MyPageMyList: React.FC = () => {
           </div>
           <IonTitle slot='start'>내 목록</IonTitle>
           <IonButtons slot='primary'>
-            {/* // TODO filter : 이야기창고, 소모임은 필터 없음*/}
             <IonButton slot='end' color='dark' routerLink='/settings'>
               <IonIcon slot='icon-only' icon={filterOutline} size='small' />
             </IonButton>
           </IonButtons>
         </IonToolbar>
+
+        <Observer>
+          {() => (
+            <>
+              <Segment
+                segments={segments}
+                selected={$segment.mylistSegment}
+                setSelected={$segment.setMyListSegment}
+              />
+
+              <FilterBar
+                show={
+                  $segment.mylistSegment === SEGMENT_KEYS.stuff ||
+                  $segment.mylistSegment === SEGMENT_KEYS.talent
+                }
+                filters={[{ name: FilterMode.type, length: filter.types.length + filter.notStatuses.length }]}
+                onReset={onResetFilter}
+                onClick={(name: string) => {
+                  setFilterMode(filterMode === name ? FilterMode.none : name)
+                }}
+              />
+            </>
+          )}
+        </Observer>
       </IonHeader>
 
-      <Segment
-        segments={segments}
-        selected={$segment.mylistSegment}
-        setSelected={$segment.setMyListSegment}
+      <FilterPopup
+        show={filterMode === FilterMode.type}
+        filterInfos={[
+          {
+            filter: filter.types,
+            items: [
+              [StuffTalentType.GIVE, '팔아요'],
+              [StuffTalentType.TAKE, '구해요'],
+            ],
+            onSelect: (newFilter) => setFilter({ ...filter, types: newFilter }),
+          },
+          {
+            filter: filter.notStatuses,
+            items: [['FINISH', '거래완료 안보기']],
+            onSelect: (newFilter) => setFilter({ ...filter, notStatuses: newFilter }),
+          },
+        ]}
+        onClose={onCloseFilterPopup}
       />
 
       <IonContent>
-        <div className='px-container my-4'>{renderList($segment.mylistSegment)}</div>
+        <div className='px-container my-4'>
+          <Observer>{() => renderList($segment.mylistSegment)}</Observer>
+        </div>
       </IonContent>
     </IonPage>
-  ))
+  )
 }
