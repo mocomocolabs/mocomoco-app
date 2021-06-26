6dd3176973877e75437c9d32314c59d5257466e3
junho <jhnam@howtobiz.com>
2021-06-26 22:43:37 +0900

    feat: 하단탭바

--- src/RouterTab.tsx
+++ src/RouterTab.tsx
@@ -3,6 +3,7 @@ import { IonReactRouter } from '@ionic/react-router'
 import { FC, useCallback, useEffect, useRef, useState } from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import { Icon } from './components/atoms/IconComponent'
+import { BottomTabMorePopover } from './components/molecules/BottomTabMorePopover'
 import { GuardRoute } from './GuardRoute'
 import { ChatPage } from './pages/ChatPage'
 import { ChatRoomPage } from './pages/ChatRoomPage'
@@ -34,117 +35,141 @@ export interface IRouterTab {
   isShow: boolean
 }
 
-type ITab = { [key: string]: { path: string; icon: string } }
+enum TAB_PATH {
+  HOME = 'home',
+  FEED = 'feed',
+  MORE = 'more',
+  CHAT = 'chat',
+  MY_PAGE = 'my-page',
+}
+type ITab = { [key: string]: { path: TAB_PATH; icon: string } }
 const TAB: ITab = {
   HOME: {
-    path: 'home',
+    path: TAB_PATH.HOME,
     icon: 'home',
   },
   FEED: {
-    path: 'feed',
+    path: TAB_PATH.FEED,
     icon: 'group',
   },
   MORE: {
-    path: 'more',
+    path: TAB_PATH.MORE,
     icon: 'send',
   },
   CHAT: {
-    path: 'chat',
+    path: TAB_PATH.CHAT,
     icon: 'message',
   },
   MY_PAGE: {
-    path: 'my-page',
+    path: TAB_PATH.MY_PAGE,
     icon: 'setting',
   },
 }
+export type IMoreTabName = 'stuff' | 'talent' | 'club' | ''
 
 export const RouterTab: FC<IRouterTab> = ({ isShow, chatUnreadCount }) => {
   const [currentTab, setCurrentTab] = useState('')
+  const [showsMore, setShowsMore] = useState(false)
+  const [isActiveMore, setIsActiveMore] = useState(false)
+  const [moreTabName, setMoreTabName] = useState<IMoreTabName>('')
 
   // eslint-disable-next-line
   const refTabbar = useRef<any>()
 
-  const getTabName = useCallback(
+  const getTabIcon = useCallback(
     (tab: ValueOf<ITab>) => {
-      const activeName = '-solid'
-      return currentTab === tab.path ? tab.icon + activeName : tab.icon
+      const activepath = '-solid'
+      return currentTab === tab.path ? tab.icon + activepath : tab.icon
     },
     [currentTab]
   )
 
+  const onTabChange = useCallback((event: { detail: { tab: string } }) => {
+    setCurrentTab(event.detail.tab)
+    setActiveMoreByPath()
+
+    if (event.detail.tab === 'more') {
+      setShowsMore(!showsMore)
+    }
+
+    // eslint-disable-next-line
+  }, [])
+
+  const setActiveMoreByPath = () => {
+    const activeTab: IMoreTabName =
+      (['club', 'talent', 'stuff'] as IMoreTabName[]).find((v) => location.pathname.includes(v)) || ''
+
+    setIsActiveMore(!!activeTab)
+    setMoreTabName(activeTab)
+  }
+
+  route.history.listen(() => {
+    setActiveMoreByPath()
+  })
+
   useEffect(() => {
     setCurrentTab(refTabbar.current?.ionTabContextState.activeTab)
+    setActiveMoreByPath()
   }, [])
 
   return (
-    <IonReactRouter history={route.history}>
-      <IonTabs ref={refTabbar}>
-        <IonRouterOutlet id='main'>
-          <Route path='/dev' component={DevPage} exact />
-          <Route path='/sign-in' component={SignInPage} exact />
-          <Route path='/sign-up' component={SignUpPage} exact />
-          <Route path='/sign-up/community' component={SignUpCommunityPage} exact />
-          <Route path='/sign-up/form' component={SignUpFormPage} exact />
-          <GuardRoute path='/home' component={HomePage} exact />
-          <GuardRoute path='/feed' component={FeedPage} exact />
-          <GuardRoute path='/feed/:id' component={FeedDetailPage} exact />
-          <GuardRoute path='/feed-write' component={FeedWritePage} exact />
-          <GuardRoute path='/stuff' component={StuffTalentPage} exact />
-          {/* <GuardRoute path='/stuff/:id' component={StuffTalentDetailPage} exact /> */}
-          <GuardRoute path='/stuff-form' component={StuffTalentFormPage} exact />
-          <GuardRoute path='/talent' component={StuffTalentPage} exact />
-          {/* <GuardRoute path='/talent/:id' component={StuffTalentDetailPage} exact /> */}
-          <GuardRoute path='/talent-form' component={StuffTalentFormPage} exact />
-          <GuardRoute path='/club' component={ClubPage} exact />
-          <GuardRoute path='/club-form' component={ClubFormPage} exact />
-          <GuardRoute path='/club/:id' component={ClubDetailPage} exact />
-          <GuardRoute path='/chat' component={ChatPage} exact />
-          <GuardRoute path='/chat/:id' component={ChatRoomPage} exact />
-          <GuardRoute path='/my-page' component={MyPage} exact />
-          <GuardRoute path='/my-page/my-list' component={MyPageMyList} exact />
-          <GuardRoute path='/my-page/like-list' component={MyPage} exact />
-          <GuardRoute path='/settings' component={SettingsPage} exact />
-          <GuardRoute path='/users/:id' component={ProfileDetailPage} exact />
-          <GuardRoute path='/users/:id/edit' component={ProfileUpdatePage} exact />
-          <Redirect from='/' to='/home' exact />
-        </IonRouterOutlet>
-        <IonTabBar
-          slot='bottom'
-          hidden={!isShow}
-          onIonTabsDidChange={(event: { detail: { tab: string } }) => {
-            setCurrentTab(event.detail.tab)
-
-            if (event.detail.tab === 'more') {
-              console.log('click more')
-            }
-          }}
-        >
-          <IonTabButton tab={TAB.HOME.path} href='/home'>
-            <Icon name={getTabName(TAB.HOME)}></Icon>
-          </IonTabButton>
-          <IonTabButton tab={TAB.FEED.path} href='/feed'>
-            <Icon name={getTabName(TAB.FEED)}></Icon>
-          </IonTabButton>
-          <IonTabButton
-            tab={TAB.MORE.path}
-            onClick={() => {
-              console.log('123123')
-            }}
-          >
-            <Icon name={getTabName(TAB.MORE)}></Icon>
-          </IonTabButton>
-          <IonTabButton tab={TAB.CHAT.path} href='/chat'>
-            <Icon name={getTabName(TAB.CHAT)}></Icon>
-            {chatUnreadCount > 0 && <div className='badge'></div>}
-          </IonTabButton>
-          {/* <IonTabButton tab='club' href='/club'>
-      <IonIcon icon={people} />
-    </IonTabButton> */}
-          <IonTabButton tab={TAB.MY_PAGE.path} href='/my-page'>
-            <Icon name={getTabName(TAB.MY_PAGE)}></Icon>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+    <>
+      <IonReactRouter history={route.history}>
+        <IonTabs ref={refTabbar}>
+          <IonRouterOutlet id='main'>
+            <Route path='/dev' component={DevPage} exact />
+            <Route path='/sign-in' component={SignInPage} exact />
+            <Route path='/sign-up' component={SignUpPage} exact />
+            <Route path='/sign-up/community' component={SignUpCommunityPage} exact />
+            <Route path='/sign-up/form' component={SignUpFormPage} exact />
+            <GuardRoute path='/home' component={HomePage} exact />
+            <GuardRoute path='/feed' component={FeedPage} exact />
+            <GuardRoute path='/feed/:id' component={FeedDetailPage} exact />
+            <GuardRoute path='/feed-write' component={FeedWritePage} exact />
+            <GuardRoute path='/stuff' component={StuffTalentPage} exact />
+            {/* <GuardRoute path='/stuff/:id' component={StuffTalentDetailPage} exact /> */}
+            <GuardRoute path='/stuff-form' component={StuffTalentFormPage} exact />
+            <GuardRoute path='/talent' component={StuffTalentPage} exact />
+            {/* <GuardRoute path='/talent/:id' component={StuffTalentDetailPage} exact /> */}
+            <GuardRoute path='/talent-form' component={StuffTalentFormPage} exact />
+            <GuardRoute path='/club' component={ClubPage} exact />
+            <GuardRoute path='/club-form' component={ClubFormPage} exact />
+            <GuardRoute path='/club/:id' component={ClubDetailPage} exact />
+            <GuardRoute path='/chat' component={ChatPage} exact />
+            <GuardRoute path='/chat/:id' component={ChatRoomPage} exact />
+            <GuardRoute path='/my-page' component={MyPage} exact />
+            <GuardRoute path='/my-page/my-list' component={MyPageMyList} exact />
+            <GuardRoute path='/my-page/like-list' component={MyPage} exact />
+            <GuardRoute path='/settings' component={SettingsPage} exact />
+            <GuardRoute path='/users/:id' component={ProfileDetailPage} exact />
+            <GuardRoute path='/users/:id/edit' component={ProfileUpdatePage} exact />
+            <Redirect from='/' to='/home' exact />
+          </IonRouterOutlet>
+          <IonTabBar slot='bottom' hidden={!isShow} onIonTabsDidChange={onTabChange}>
+            <IonTabButton tab={TAB.HOME.path} href='/home'>
+              <Icon name={getTabIcon(TAB.HOME)}></Icon>
+            </IonTabButton>
+            <IonTabButton tab={TAB.FEED.path} href='/feed'>
+              <Icon name={getTabIcon(TAB.FEED)}></Icon>
+            </IonTabButton>
+            <IonTabButton tab={TAB.MORE.path} selected={isActiveMore}>
+              <Icon name={isActiveMore ? TAB.MORE.icon + '-solid' : TAB.MORE.icon}></Icon>
+            </IonTabButton>
+            <IonTabButton tab={TAB.CHAT.path} href='/chat'>
+              <Icon name={getTabIcon(TAB.CHAT)}></Icon>
+              {chatUnreadCount > 0 && <div className='badge'></div>}
+            </IonTabButton>
+            <IonTabButton tab={TAB.MY_PAGE.path} href='/my-page'>
+              <Icon name={getTabIcon(TAB.MY_PAGE)}></Icon>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+      <BottomTabMorePopover
+        isOpen={showsMore}
+        setIsOpen={setShowsMore}
+        activeTab={moreTabName}
+      ></BottomTabMorePopover>
+    </>
   )
 }
---
+++ src/components/molecules/BottomTabMorePopover.tsx
@@ -0,0 +1,53 @@
+import { IonPopover } from '@ionic/react'
+import { Dispatch, FC, SetStateAction } from 'react'
+import { IMoreTabName } from '../../RouterTab'
+import { route } from '../../services/route-service'
+import { TextBase } from '../atoms/TextBaseComponent'
+import './BottomTabMorePopover.scss'
+
+export interface IBottomTabMorePopover {
+  isOpen: boolean
+  activeTab: IMoreTabName
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export const BottomTabMorePopover: FC<IBottomTabMorePopover> = ({ isOpen, setIsOpen, activeTab }) => {
+  return (
+    <IonPopover
+      cssClass='bottom-tab-more-popover'
+      isOpen={isOpen}
+      showBackdrop={false}
+      onDidDismiss={(e: any) => setIsOpen(e)}
+    >
+      <ul className='p-4'>
+        <li
+          className='text-center pv-2'
+          onClick={() => {
+            route.stuff()
+            setIsOpen(false)
+          }}
+        >
+          <TextBase className={`text-bold ${activeTab === 'stuff' && 'primary'}`}>물건창고</TextBase>
+        </li>
+        <li
+          className='text-center pv-2'
+          onClick={() => {
+            route.talent()
+            setIsOpen(false)
+          }}
+        >
+          <TextBase className={`text-bold ${activeTab === 'talent' && 'primary'}`}>재능창고</TextBase>
+        </li>
+        <li
+          className='text-center pv-2'
+          onClick={() => {
+            route.clubs()
+            setIsOpen(false)
+          }}
+        >
+          <TextBase className={`text-bold ${activeTab === 'club' && 'primary'}`}>소모임</TextBase>
+        </li>
+      </ul>
+    </IonPopover>
+  )
+}
