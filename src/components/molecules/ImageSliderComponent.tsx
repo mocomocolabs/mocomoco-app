caa22b0bfeba7121a7b65fc223d90759773bf6a3
junho <jhnam@howtobiz.com>
2021-07-14 23:52:13 +0900

    update: 회원가입 하마 이미지 변경

--- src/components/molecules/ImageSliderComponent.tsx
+++ src/components/molecules/ImageSliderComponent.tsx
@@ -5,6 +5,7 @@ import { ImageWithCorner } from './ImageWithCorner'
 
 export interface IImageSlider {
   urls: string[]
+  height?: number
 }
 
 const slideOpts = {
@@ -12,12 +13,12 @@ const slideOpts = {
   speed: 400,
 }
 
-export const ImageSlider: FC<IImageSlider> = ({ urls }) =>
+export const ImageSlider: FC<IImageSlider> = ({ urls, height = 337 }) =>
   urls.length ? (
     <IonSlides key={urls.join('_')} pager={urls.length > 1} options={slideOpts}>
       {urls?.map((v, i) => (
         <IonSlide key={i}>
-          <ImageWithCorner height={337} url={v} />
+          <ImageWithCorner height={height} url={v} />
         </IonSlide>
       ))}
     </IonSlides>
