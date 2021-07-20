13a97386f61fff20fcb92a8c0adb99c538a850c2
gyuon <onssss@gmail.com>
2021-07-20 17:45:40 +0900

    fix: timeDiff 로직 오류 수정

--- src/utils/datetime-util.ts
+++ src/utils/datetime-util.ts
@@ -15,20 +15,20 @@ export const timeDiff = (from?: string, to?: string) => {
   const fromDate = from ? dayjs(from) : dayjs()
   const toDate = to ? dayjs(to) : dayjs()
 
-  const diffYears = fromDate.diff(toDate, 'year')
-  if (diffYears > 0) return toDate.format('YYYY년 M월 D일')
+  const diffYears = toDate.diff(fromDate, 'year')
+  if (diffYears > 0) return fromDate.format('YYYY년 M월 D일')
 
-  const diffDays = fromDate.diff(toDate, 'day')
-  if (diffDays > 6) return toDate.format('M월 D일')
+  const diffDays = toDate.diff(fromDate, 'day')
+  if (diffDays > 6) return fromDate.format('M월 D일')
   if (diffDays > 0) return `${diffDays}일 전`
 
-  const diffHours = fromDate.diff(toDate, 'hour')
+  const diffHours = toDate.diff(fromDate, 'hour')
   if (diffHours > 0) return `${diffHours}시간 전`
 
-  const diffMinutes = fromDate.diff(toDate, 'minute')
+  const diffMinutes = toDate.diff(fromDate, 'minute')
   if (diffMinutes > 0) return `${diffMinutes}분 전`
 
-  const diffSeconds = fromDate.diff(toDate, 'second')
+  const diffSeconds = toDate.diff(fromDate, 'second')
   if (diffSeconds > 0) return `${diffSeconds}초 전`
 
   return '방금 전'
