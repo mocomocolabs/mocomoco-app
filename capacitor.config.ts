36851f30cc13813f2f3a2e2fa416371fbcadb46f
gyuon <onssss@gmail.com>
2021-08-04 17:26:03 +0900

    build: gradle v7 적용, capacitor.config.ts 사용하도록 변경

---
+++ capacitor.config.ts
@@ -0,0 +1,34 @@
+/* TODO apply later
+/// <reference types="@capacitor/local-notifications" />
+/// <reference types="@capacitor/push-notifications" />
+/// <reference types="@capacitor/splash-screen" />
+*/
+
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.mocomoco.hama',
+  appName: 'hama',
+  webDir: 'build',
+  bundledWebRuntime: false,
+  loggingBehavior: 'production', // TODO change to 'debug' when release
+  android: {
+    allowMixedContent: true, // TODO change to false when release
+  },
+
+  // TODO apply later
+  // plugins: {
+  //   SplashScreen: {
+  //     launchAutoHide: false,
+  //   },
+  //   LocalNotifications: {
+  //     smallIcon: 'ic_stat_icon_config_sample',
+  //     iconColor: '#CE0B7C',
+  //   },
+  //   PushNotifications: {
+  //     presentationOptions: ["alert", "sound"]
+  //   }
+  // },
+}
+
+export = config
