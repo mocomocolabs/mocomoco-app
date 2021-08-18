d0da2625c961a447eacac548b957dd3460392bde
gyuon <onssss@gmail.com>
2021-08-18 20:56:30 +0900

    update: 물건재능목록 - Header 정리

--- src/pages/StuffTalentPage.tsx
+++ src/pages/StuffTalentPage.tsx
@@ -1,14 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonToolbar } from '@ionic/react'
+import { IonContent, IonPage } from '@ionic/react'
 import { reaction } from 'mobx'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useLocation } from 'react-router'
 import { Icon } from '../components/atoms/IconComponent'
 import { Pad } from '../components/atoms/PadComponent'
-import { Searchbar, SearchbarChangeEventDetail } from '../components/atoms/SearchbarComponent'
-import { BackButton } from '../components/molecules/BackButtonComponent'
 import { CommunitySelector } from '../components/molecules/CommunitySelectorComponent'
 import { FilterBar } from '../components/molecules/FilterBarComponent'
 import { FilterPopup } from '../components/molecules/FilterPopupComponent'
+import { Header } from '../components/organisms/HeaderComponent'
 import { StuffTalentList } from '../components/organisms/StuffTalentListComponent'
 import { useStore } from '../hooks/use-store'
 import { getPageKey, routeFunc, typeLabels } from '../models/stufftalent'
@@ -17,7 +16,7 @@ import { IStuffTalentFilter, StuffTalentPageKey, StuffTalentStatus } from '../mo
 const FilterMode = { none: 'none', category: '카테고리', type: '거래유형' }
 type FilterMode = typeof FilterMode[keyof typeof FilterMode]
 
-const initialSearch = ''
+const initialSearchValue = ''
 
 export const StuffTalentPage: React.FC = () => {
   const { $stuff, $talent, $ui, $community } = useStore()
@@ -30,9 +29,8 @@ export const StuffTalentPage: React.FC = () => {
   const { store } = pageData[getPageKey(useLocation().pathname)]
   const { routeForm } = routeFunc[store.predefined.pageKey]
 
-  const [searchMode, setSearchMode] = useState(false)
-  const [search, setSearch] = useState(initialSearch)
-  const onSearchSubmit = (e: CustomEvent<SearchbarChangeEventDetail>) => setSearch(e.detail.value!)
+  const [showSearch, setShowSearch] = useState(false)
+  const [searchValue, setSearchValue] = useState(initialSearchValue)
 
   const initialFilter: IStuffTalentFilter = {
     isPublic: $community.selectedId !== store.$auth.user.communityId ? true : undefined,
@@ -61,7 +59,6 @@ export const StuffTalentPage: React.FC = () => {
     const disposeReaction = reaction(
       () => $community.selectedId,
       (selectedId) => {
-        // TODO 모든 공동체 선택 시 isPublic=true 처리 필요
         setFilter({
           ...filter,
           communityId: selectedId,
@@ -76,77 +73,84 @@ export const StuffTalentPage: React.FC = () => {
   }, [])
 
   useEffect(() => {
-    !searchMode && setSearch(initialSearch)
+    !showSearch && setSearchValue(initialSearchValue)
 
     return function cleanup() {
-      setSearch(initialSearch)
+      setSearchValue(initialSearchValue)
     }
-  }, [searchMode])
+  }, [showSearch])
 
-  const searchbarRef = useRef<HTMLIonSearchbarElement>(null)
-  const onSearchIconClick = useCallback(() => {
-    setSearchMode(true)
-    setTimeout(async () => searchbarRef.current?.setFocus(), 100)
-  }, [])
+  const headerIcons = useMemo(
+    () => (
+      <div className='flex gap-4'>
+        <div onClick={() => setShowSearch(true)}>
+          <Icon name='search' />
+        </div>
+        <div
+          onClick={() => {
+            const isWriting = store.form.title || store.form.content || store.form.images?.length
+
+            if (isWriting) {
+              return $ui.showAlert({
+                message: '작성하던 글이 있어요. 이어서 작성하시겠어요?',
+                onSuccess() {
+                  routeForm()
+                },
+                onFail() {
+                  store.resetForm()
+                  routeForm()
+                },
+              })
+            }
+
+            routeForm()
+          }}
+        >
+          <Icon name='pencil' />
+        </div>
+      </div>
+    ),
+    []
+  )
+
+  const filterBarComponent = useMemo(
+    () => (
+      <div className='px-container mt-1'>
+        <FilterBar
+          filters={[
+            { name: FilterMode.category, length: filter.categories.length },
+            { name: FilterMode.type, length: filter.types.length + filter.notStatuses.length },
+          ]}
+          onReset={onResetFilter}
+          onClick={(name: string) => {
+            setFilterMode(filterMode === name ? FilterMode.none : name)
+          }}
+        />
+
+        <Pad className='h-3' />
+      </div>
+    ),
+    [filterMode, filter]
+  )
+
+  const searchbar = useMemo(
+    () => ({
+      show: showSearch,
+      setShow: (show: boolean) => setShowSearch(show),
+      value: searchValue,
+      setValue: (search: string) => setSearchValue(search),
+    }),
+    [showSearch, searchValue]
+  )
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <div className='flex-between-center min-height-56' hidden={searchMode}>
-            <div slot='start'>
-              <CommunitySelector></CommunitySelector>
-            </div>
-            <div className='flex' slot='end'>
-              <div onClick={onSearchIconClick}>
-                <Icon name='search' className='ml-4' />
-              </div>
-              <div
-                onClick={() => {
-                  const isWriting = store.form.title || store.form.content || store.form.images?.length
-
-                  if (isWriting) {
-                    return $ui.showAlert({
-                      message: '작성하던 글이 있어요. 이어서 작성하시겠어요?',
-                      onSuccess() {
-                        routeForm()
-                      },
-                      onFail() {
-                        store.resetForm()
-                        routeForm()
-                      },
-                    })
-                  }
-
-                  routeForm()
-                }}
-              >
-                <Icon name='pencil' className='ml-4' />
-              </div>
-            </div>
-          </div>
-
-          <div className='flex-between-center' hidden={!searchMode}>
-            <BackButton type='arrow' action={() => setSearchMode(false)} />
-            <Searchbar ref={searchbarRef} value={search} onSearchSubmit={onSearchSubmit} />
-          </div>
-        </IonToolbar>
-
-        <div className='px-container mt-1'>
-          <FilterBar
-            filters={[
-              { name: FilterMode.category, length: filter.categories.length },
-              { name: FilterMode.type, length: filter.types.length + filter.notStatuses.length },
-            ]}
-            onReset={onResetFilter}
-            onClick={(name: string) => {
-              setFilterMode(filterMode === name ? FilterMode.none : name)
-            }}
-          />
-
-          <Pad className='h-3' />
-        </div>
-      </IonHeader>
+      <Header
+        start={<CommunitySelector />}
+        end={headerIcons}
+        bottom={filterBarComponent}
+        searchbar={searchbar}
+      />
 
       <div className='-mt-3' hidden={filterMode !== FilterMode.category}>
         <FilterPopup
@@ -181,7 +185,7 @@ export const StuffTalentPage: React.FC = () => {
 
       <IonContent>
         <div className='px-container mt-1 mb-4'>
-          <StuffTalentList store={store} search={search} filter={filter} />
+          <StuffTalentList store={store} search={searchValue} filter={filter} />
         </div>
       </IonContent>
     </IonPage>
