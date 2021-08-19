dd7453f1ef65942d09f1b5e601feca79504e391a
gyuon <onssss@gmail.com>
2021-08-19 17:07:20 +0900

    update: ImageWithCorner classname에 undefined 표시안함

--- src/components/molecules/ImageWithCorner.tsx
+++ src/components/molecules/ImageWithCorner.tsx
@@ -29,30 +29,24 @@ export const ImageWithCorner: FC<IImageWithCorner> = ({
   tailRight = false,
   dark = false,
 }) => {
-  const {
-    outerDivClass,
-    imageClass,
-    cornerClass,
-    cornerStyle,
-    tailDivClass,
-    tailDivStyle,
-  }: IStyle = tailRight
-    ? {
-        outerDivClass: 'inline-flex relative w-full',
-        imageClass: `br-l-xxlg ${dark && 'dark'}`,
-        cornerClass: 'icon-rotate-270 absolute top-0',
-        cornerStyle: { right: radiusSize, width: radiusSize, height: radiusSize },
-        tailDivClass: 'z-10 w-full h-full bg-white absolute right-0 right-radius',
-        tailDivStyle: { width: radiusSize, borderBottomLeftRadius: radiusSize },
-      }
-    : {
-        outerDivClass: 'relative w-full',
-        imageClass: `${isRoundTop && 'br-t-xxlg'} ${dark && 'dark'}`,
-        cornerClass: 'absolute right-0',
-        cornerStyle: { top: height - radiusSize * 2, width: radiusSize, height: radiusSize },
-        tailDivClass: 'z-10 w-full bg-white absolute bottom-0 bottom-radius',
-        tailDivStyle: { height: radiusSize, borderTopLeftRadius: radiusSize },
-      }
+  const { outerDivClass, imageClass, cornerClass, cornerStyle, tailDivClass, tailDivStyle }: IStyle =
+    tailRight
+      ? {
+          outerDivClass: 'inline-flex relative w-full',
+          imageClass: `br-l-xxlg ${dark ? 'dark' : ''}`,
+          cornerClass: 'icon-rotate-270 absolute top-0',
+          cornerStyle: { right: radiusSize, width: radiusSize, height: radiusSize },
+          tailDivClass: 'z-10 w-full h-full bg-white absolute right-0 right-radius',
+          tailDivStyle: { width: radiusSize, borderBottomLeftRadius: radiusSize },
+        }
+      : {
+          outerDivClass: 'relative w-full',
+          imageClass: `${isRoundTop ? 'br-t-xxlg' : ''} ${dark ? 'dark' : ''}`,
+          cornerClass: 'absolute right-0',
+          cornerStyle: { top: height - radiusSize * 2, width: radiusSize, height: radiusSize },
+          tailDivClass: 'z-10 w-full bg-white absolute bottom-0 bottom-radius',
+          tailDivStyle: { height: radiusSize, borderTopLeftRadius: radiusSize },
+        }
 
   return (
     <div className={outerDivClass}>
