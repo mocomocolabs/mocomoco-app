86c06b1b2742f741c8835877746f729417f1674f
gyuon <onssss@gmail.com>
2021-09-08 22:29:03 +0900

    update: Toast 위치 약간 높임, 텍스트 가운데정렬

--- src/components/atoms/ToastComponent.tsx
+++ src/components/atoms/ToastComponent.tsx
@@ -2,11 +2,18 @@ import { IonToast } from '@ionic/react'
 import { useObserver } from 'mobx-react-lite'
 import { FC } from 'react'
 import { useStore } from '../../hooks/use-store'
+import './ToastComponent.scss'
 
 export const Toast: FC = () => {
   const { $ui } = useStore()
 
   return useObserver(() => (
-    <IonToast isOpen={$ui.toast.isOpen} color={$ui.toast.color} message={$ui.toast.message}></IonToast>
+    <IonToast
+      isOpen={$ui.toast.isOpen}
+      color={$ui.toast.color}
+      message={$ui.toast.message}
+      position='bottom'
+      cssClass='bottom-toast'
+    />
   ))
 }
