38e0383f02c062724e48bc8c0531268897c9ae50
gyuon <onssss@gmail.com>
2021-09-18 21:11:33 +0900

    update: 문구수정 - 구름씨가이드 => 하마 가이드

--- src/components/molecules/MypageColumnListComponent.tsx
+++ src/components/molecules/MypageColumnListComponent.tsx
@@ -4,7 +4,7 @@ export const MypageColumnList: React.FC = () => {
   return (
     <div className='flex-col ml-1'>
       <MypageColumnItem icon='invite' title='초대' href='' disabled />
-      <MypageColumnItem icon='guide' title='구름씨가이드' href='' disabled />
+      <MypageColumnItem icon='guide' title='하마 가이드' href='' disabled />
       <MypageColumnItem icon='search' title='자주 묻는 질문' href='' disabled />
       <MypageColumnItem icon='notice' title='공지사항' href='' disabled />
       <MypageColumnItem icon='setting' title='설정' href='/tabs/my-page/settings' />
