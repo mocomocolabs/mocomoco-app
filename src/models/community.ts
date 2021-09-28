bb10397461f2a17da1bd3f8a6e7813f2735e2eba
gyuon <onssss@gmail.com>
2021-09-28 14:47:50 +0900

    fix: community.userCount - 승인된 사용자수만 카운팅함

--- src/models/community.ts
+++ src/models/community.ts
@@ -1,17 +1,22 @@
 import _ from 'lodash'
 import { ICommunityDto } from '../stores/community-store.d'
 import { ICommunity } from './community.d'
+import { SIGN_UP_STATUS } from './sign-up.d'
 import { User } from './user'
 
 export class Community {
   static of(dto: ICommunityDto): ICommunity {
-    return !!dto && !_.isEmpty(dto)
-      ? {
-          ...dto,
-          users: dto.users?.map((u) => User.of(u)),
-          userCount: dto.users?.filter((u) => u.isUse).length ?? 0,
-          bannerUrl: dto.atchFiles?.[0]?.url,
-        }
-      : ({} as ICommunity)
+    if (!!!dto || _.isEmpty(dto)) {
+      return {} as ICommunity
+    }
+
+    const approvedUsers = dto.users?.filter((u) => u.isUse && u.status === SIGN_UP_STATUS.승인)
+
+    return {
+      ...dto,
+      users: approvedUsers?.map((u) => User.of(u)),
+      userCount: approvedUsers?.length ?? 0,
+      bannerUrl: dto.atchFiles?.[0]?.url,
+    }
   }
 }
