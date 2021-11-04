4b904f614e1cc5c23c7bf7097e5e8e5e691973db
gyuon <onssss@gmail.com>
2021-11-05 07:01:38 +0900

    fix: 댓글 모두 삭제한 상태일 때 댓글수 아이콘이 활성화(색칠)되어 있음

--- src/models/feed.ts
+++ src/models/feed.ts
@@ -9,20 +9,24 @@ export interface Feed extends IFeed {}
 
 export class Feed {
   static of(dto: IFeedDto, userId: number): IFeed {
-    return !!dto && !_.isEmpty(dto)
-      ? {
-          ...dto,
-          user: User.of(dto.user),
-          schedule: dto.schedule?.isUse ? this.scheduleOf(dto.schedule) : undefined,
-          imageUrls: dto.atchFiles.map((v) => v.url),
-          comments: dto.feedComments
-            .filter((v) => v.isUse)
-            .sort((a, b) => Number.parseInt(a.createdAt) - Number.parseInt(b.createdAt))
-            .map((c) => Comment.of(c)),
-          likeCount: dto.feedUsers.filter((v) => v.isLike).length,
-          writtenComment: dto.feedComments.some((v) => v.user.id === userId),
-        }
-      : ({} as IFeed)
+    if (!!!dto || _.isEmpty(dto)) {
+      return {} as IFeed
+    }
+
+    const comments = dto.feedComments
+      .filter((v) => v.isUse)
+      .sort((a, b) => Number.parseInt(a.createdAt) - Number.parseInt(b.createdAt))
+      .map((c) => Comment.of(c))
+
+    return {
+      ...dto,
+      user: User.of(dto.user),
+      schedule: dto.schedule?.isUse ? this.scheduleOf(dto.schedule) : undefined,
+      imageUrls: dto.atchFiles.map((v) => v.url),
+      comments,
+      likeCount: dto.feedUsers.filter((v) => v.isLike).length,
+      writtenComment: comments.some((v) => v.user.id === userId),
+    }
   }
 
   static scheduleOf(dto: IFeedScheduleDto): IFeedSchedule {
